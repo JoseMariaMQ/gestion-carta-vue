f6f6c730cf05626cfd7a4a0cee6803fe9c78ddb1
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-05-12 09:39:10 +0200

    Hide dishes, desserts, and drinks hidden from the client page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,11 +14,11 @@
       <span>S</span>
     </h1>
     <a v-for="contact in contacts" :key="contact.id" :href="contact.url" target="_blank" rel="noreferrer" class="mx-2">
-      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'whatsapp'" :icon="['fab', 'whatsapp-square']" class="display-6"/>
-      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'phone'" :icon="['fas', 'phone-square']" class="display-6"/>
-      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'facebook'" :icon="['fab', 'facebook-square']" class="display-6"/>
-      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'instagram'" :icon="['fab', 'instagram-square']" class="display-6"/>
-      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'google maps'" :icon="['fas', 'map-marker-alt']" class="display-6"/>
+      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'whatsapp' && contact.url" :icon="['fab', 'whatsapp-square']" class="display-6"/>
+      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'phone' && contact.url" :icon="['fas', 'phone-square']" class="display-6"/>
+      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'facebook' && contact.url" :icon="['fab', 'facebook-square']" class="display-6"/>
+      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'instagram' && contact.url" :icon="['fab', 'instagram-square']" class="display-6"/>
+      <FontAwesomeIcon v-if="contact.name.toLowerCase() === 'google maps' && contact.url" :icon="['fas', 'map-marker-alt']" class="display-6"/>
     </a>
 
 
